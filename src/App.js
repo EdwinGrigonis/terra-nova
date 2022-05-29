dd930910e8f65976e019421ca4336201cbbe4fec
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-05-29 16:15:42 +0900

    added inactive routes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      hi there
-    </div>
+    <Router>
+          <div className=''>
+              <Navbar />
+              <main className=''>
+                <Alert />
+                <Routes>
+                  <Route path='/' element={< Home />} />
+                  <Route path='/about' element={< About />} />
+                  <Route path='/user/:login' element={<User />}></Route>
+                  <Route path='/notfound' element={< NotFound />} />
+                  <Route path='/*' element={< NotFound />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+        </Router>
   );
 }
 
