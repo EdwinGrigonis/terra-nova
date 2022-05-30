32692880c00b84857d2ce6c039fccbff48717056
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-05-30 17:03:48 +0900

    added tailwind, daisyui, navbar, stylings

--- src/components/assets/Navbar.jsx
+++ src/components/assets/Navbar.jsx
@@ -4,19 +4,28 @@ import { PropTypes } from 'prop-types'
 
 function Navbar({title}) {
     return (
-        <nav className=''>
-            <div className=''>
-                <div className=''>
-                    <Link to='/' className=''>
+        <nav className='navbar mb-12 shadow-lg bg-secondary text-neutral-content'>
+            <div className='container mx-auto'>
+                <div className='flex-none px-2 mx-2'>
+                    <Link to='/' className='text-3xl font-bold align-middle'>
                         {title}
                     </Link>
                 </div>
-                <div className=''>
-                    <div className=''>
-                        <Link to='/' className=''>
+                <div className='flex-1 px-2 mx-2'>
+                    <div className='md:flex sm:dropdown justify-end'>
+                        <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
                             Home
                         </Link>
-                        <Link to='/about' className=''>
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+                            About
+                        </Link>
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+                            About
+                        </Link>
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+                            About
+                        </Link>
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
                             About
                         </Link>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js, jsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
