1bab9bc616838e277d353e0ad944ae0a7181500f
Edwin Grigonis <edwin.grigonis2@mail.dcu.ie>
2022-06-02 16:23:57 +0900

    modified styles

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ import teamlg from '../images/team-lg.jpg';
 function Home() {
   return (
     <div className='container text-center'>
-        <img src={teamlg} alt="scotts team" className='homeimage rounded-xl mx-10 w-50 shadow-lg'/>
+        <img src={teamlg} alt="scotts team" className='homeimage sm:rounded-xl mx-10 w-50 shadow-lg'/>
         Home
     </div>
   )
